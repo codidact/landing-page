839fba7e309e517d823bf3399f88e62f74e2c56a
Matthew Brent <mattjbrent@gmail.com>
2020-01-17 20:01:36 -0500

    Stub webpack prod config file
    Add mini css extract plugin

---
+++ config/prod.webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable-next-line */
+const webpack = require("webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "../src/js/index.js"),
+
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "assets/js/[name].js",
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "sass-loader" }],
+            },
+        ],
+    },
+    devServer: {
+        contentBase: ["./dist"],
+        port: 3000,
+    },
+    devtool: "cheap-eval-source-map",
+    watch: true,
+};
